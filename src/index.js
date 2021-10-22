e56bb23caea342d21663ffc193e8997f23e5f24e
Michael DeSantis <m.desantis628@gmail.com>
2021-10-21 22:20:04 -0400

    code refactor. css/html done. working on js logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import './styles/style.css'
+const headerButton = document.querySelectorAll('[data-header-btn]')
+const projectList = document.querySelector('[data-project-list]')
+const taskList = document.querySelector('[data-task-list]')
+const dueByList = document.querySelector('[data-due-by-list]')
+const addTaskForm = document.querySelector('[data-add-task-form]')
+const addProjectForm = document.querySelector("[data-add-project-form]")
+const dataTaskInput = document.querySelector('[data-task-input]')
+const dataDateInput = document.querySelector('[data-date-input]')
+const dataProjectInput = document.querySelector('[data-project-input]')
+const dataPriorityInput = document.querySelector('[data-priority-input]')
+
+let projects = ['Clean', 'Work', 'Weekend', 'Dinner']
 
 function dom(element, attributes = {}, text, parent) {
     const elem = document.createElement(element);
@@ -14,109 +25,37 @@ function dom(element, attributes = {}, text, parent) {
     return elem;
 }
 
-const entry = document.querySelector('.to-do-entry')
-const inputOne = document.querySelector('#input-one')
-const inputTwo = document.querySelector('#input-two')
-const content = document.querySelector(".content");
-const addTask = document.querySelector('.add-task-form')
-const addProject = document.querySelector('.add-project-form')
-const inputProject = document.querySelector('.input-project')
-const projectList = document.querySelector('[data-project-list]')
+function classOn(elem, classList) {
+    elem.classList.add(classList)
+}
 
-
-
-const LOCAL_STORAGE_PROJECT_KEY = 'task.projects'
-let projects = JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECT_KEY)) || []
-
-// newTaskForm.addEventListener('submit', e => {
-//     e.preventDefault()
-//     const taskName = newTaskInput.value
-//     if (taskName == null || taskName === '') return
-//     const task = createTask(taskName)
-//     newTaskInput.value = null
-//     const selectedList = lists.find(list => list.id === selectedListId)
-//     selectedList.tasks.push(task)
-//     saveAndRender()
-// })
+function classOff(elem, classList) {
+    elem.classList.add(classList)
+}
 
 function render() {
-    projects.forEach(project => {
-        dom('option', {
-                id: Date.now().toString()
-            },
-            project,
-            projectList
-        )
+    clearElement(projectList)
+    projects.forEach(item => {
+        dom('li', {
+            classList: 'project-name',
+            id: item.id
+        }, item.name, projectList)
     })
 }
 
-function tasker(theTask, date) {
-    dom('div', {
-        classList: 'task'
-    }, theTask, entry)
-    dom('div', {
-        id: Date.now().toString()
-    }, date, entry)
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
 }
-
-function projectHelper(theProject) {
-    dom('option', {
-            id: Date.now().toString()
-        },
-        theProject,
-        projectList
-    )
-}
-
-function clearTaskForm() {
-    inputOne.value = null
-    inputTwo.value = null
-}
-
-function clearProjectForm() {
-    inputProject.value = null
-}
-
-const btnHandler = document.querySelectorAll("button");
-btnHandler.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        if (btn.id == "add-task") {
-            addTask.classList.toggle('show')
-        }
-        if (btn.id == "add-project") {
-            addProject.classList.toggle('show')
-        }
-    });
-});
-
-addTask.addEventListener('submit', (e) => {
-    e.preventDefault()
-    addTask.classList.toggle('show')
-    dom("div", {
-        classList: "to-do-entry"
-    }, null, content);
-    tasker(inputOne.value, inputTwo.value)
-    clearTaskForm()
-})
-
-addProject.addEventListener('submit', (e) => {
-    e.preventDefault()
-    addProject.classList.toggle('show')
-    projects.push(projectHelper(inputProject.value))
-    clearProjectForm()
-    render()
-})
 
 class Project {
     constructor(name) {
-        this.name = name;
-        this.tasks = [];
+        this.name = name
+        this.tasks = []
+        this.id = Date.now()
+        this.classList = 'project-name'
     }
 }
 
-class Task {
-    constructor(name, dueDate) {
-        this.name = name;
-        this.dueDate = dueDate
-    }
-}
+render()
