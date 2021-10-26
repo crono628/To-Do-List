c5ecebdfe78265d8ac40a0e14a62b1f952baf984
Michael DeSantis <m.desantis628@gmail.com>
2021-10-26 19:32:55 -0400

    app functioning 90%. IC before spread to files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import './styles/style.css';
-
 const headerButton = document.querySelectorAll("[data-header-btn]");
 const projectList = document.querySelector("[data-project-list]");
 const taskList = document.querySelector("[data-task-list]");
@@ -11,12 +9,12 @@ const dataCancelFormBtn = document.querySelectorAll("[data-form-cancel]");
 const dataTaskInput = document.querySelector("[data-task-input]");
 const dataDateInput = document.querySelector("[data-date-input]");
 const dataProjectInput = document.querySelector("[data-project-input]");
-const dataDeleteProjectBtn = document.querySelectorAll("[data-delete-project]")
-const dataProjectIdentification = document.querySelectorAll("[data-project-id]")
+const dataDeleteProjectBtn = document.querySelectorAll("[data-delete-project]");
 
-let ls = require("local-storage")
+let ls = require("local-storage");
 let projects = JSON.parse(ls("todo.projects")) || []
 let currentProjectId = JSON.parse(ls("todo.currentProjectId"))
+
 
 function dom(element, attributes = {}, text, parent) {
     const elem = document.createElement(element);
@@ -33,52 +31,71 @@ function dom(element, attributes = {}, text, parent) {
 }
 
 function render() {
-    clearLists(projectList)
-    renderProjects()
-    const currentSelection = findCurrentProject(projects)
-    renderTasks(currentSelection)
-    closeForms()
+    clearLists(projectList);
+    clearTasks();
+    renderProjects();
+    closeForms();
 }
 
 function renderProjects() {
-    projects.forEach(project => {
-        let obj = dom('div', {
-            classList: project.classList,
-            id: project.id
-        }, project.name, projectList)
-        obj.dataset.arrayIndex = getIndex(project)
-        obj.addEventListener('click', (e) => {
-            currentProjectId = e.target.id
-            projectList.childNodes.forEach(child => {
-                child.classList.remove("active")
-            })
-            obj.classList.add("active")
-        })
+    clearLists(projectList);
+    projects.forEach((project) => {
+        let obj = dom(
+            "div", {
+                classList: project.classList,
+                id: project.id
+            },
+            project.name,
+            projectList
+        );
+        obj.dataset.arrayIndex = getIndex(project);
+        obj.addEventListener("click", (e) => {
+            currentProjectId = e.target.id;
+            projectList.childNodes.forEach((child) => {
+                child.classList.remove("active");
+            });
+            obj.classList.add("active");
+            clearTasks();
+            renderTasks();
+        });
     });
 }
 
-function renderTasks(currentProject) {
-    currentProject.tasks.forEach(listItem => {
-        let task = dom('div', {
-            classList: listItem.classList,
-            id: listItem.id
-        }, listItem.name, taskList)
-        dom('div', {
-            classList: listItem.classList,
-            id: listItem.id
-        }, listItem.dueDate, dueByList)
-        dom('div', {
-            classList: listItem.classList,
-            id: listItem.id
-        }, listItem.complete, checkboxList)
-        task.dataset.arrayIndex = getIndex(project)
-        task.addEventListener('click', (e) => {
-            taskList.childNodes.forEach(child => {
-                child.classList.remove("complete")
-            })
-            task.classList.add("complete")
-        })
-    });
+function renderTasks() {
+    if (currentProjectId) {
+        clearTasks();
+        let theThing = findCurrentProject(projects);
+        theThing.tasks.forEach((listItem) => {
+            let task = dom(
+                "div", {
+                    classList: listItem.classList,
+                    id: currentProjectId
+                },
+                listItem.name,
+                taskList
+            );
+            dom(
+                "div", {
+                    classList: listItem.classList,
+                    id: currentProjectId
+                },
+                listItem.dueDate,
+                dueByList
+            );
+            dom(
+                "div", {
+                    classList: listItem.classList,
+                    id: currentProjectId
+                },
+                listItem.complete,
+                checkboxList
+            );
+            task.dataset.arrayIndex = getIndex(theThing);
+            task.addEventListener("click", (e) => {
+                task.classList.toggle("complete");
+            });
+        });
+    }
 }
 
 function save() {
@@ -87,113 +104,135 @@ function save() {
 }
 
 function renderAndSave() {
-    render()
-    save()
+    render();
+    save();
+}
+
+function newProjectRender() {
+    let list = document.getElementsByClassName("project-name");
+    for (let i = 0; i < projects.length - 1; i++) {
+        list[i].classList.remove("active");
+    }
+}
+
+function deleteButtonRender() {
+    let list = document.getElementsByClassName("project-name");
+    for (let i = 0; i < projects.length; i++) {
+        list[i].classList.remove("active");
+    }
 }
 
 function clearLists(element) {
     while (element.firstChild) {
-        element.removeChild(element.firstChild)
+        element.removeChild(element.firstChild);
     }
 }
 
+function clearTasks() {
+    clearLists(dueByList);
+    clearLists(taskList);
+    clearLists(checkboxList);
+}
+
 function closeForms() {
-    dataForms.forEach(form => {
-        form.classList.add('hide')
-    })
+    dataForms.forEach((form) => {
+        form.classList.add("hide");
+    });
 }
 
 function findCurrentProject(arr) {
-    let finder = arr.find(item => item.id === currentProjectId)
-    return finder
+    let finder = arr.find((item) => item.id === currentProjectId);
+    return finder;
 }
 
 function getIndex(thing) {
-    return projects.indexOf(thing)
+    return projects.indexOf(thing);
 }
 
-headerButton.forEach(btn => {
-    btn.addEventListener('click', () => {
-        dataForms.forEach(form => {
-            form.classList.add('hide')
-        })
+headerButton.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        dataForms.forEach((form) => {
+            form.classList.add("hide");
+        });
         if (btn.dataset.headerBtn == "project") {
-            document.querySelector('.add-project-form').classList.remove('hide')
+            document.querySelector(".add-project-form").classList.remove("hide");
         }
         if (btn.dataset.headerBtn == "task") {
-            document.querySelector('.add-task-form').classList.remove('hide')
+            document.querySelector(".add-task-form").classList.remove("hide");
         }
-    })
-})
+    });
+});
 
-dataSubmitFormBtn.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        e.preventDefault()
+dataSubmitFormBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
         if (btn.dataset.formSubmit == "project") {
-            if (dataProjectInput.value == null || dataProjectInput == '') {
-                return
+            if (dataProjectInput.value === null || dataProjectInput === " ") {
+                return;
             } else {
-                let newObj = new Project(dataProjectInput.value)
-                projects.push(newObj)
-                currentProjectId = newObj.id
-                dataProjectInput.value = null
-                renderAndSave()
+                let newObj = new Project(dataProjectInput.value);
+                projects.push(newObj);
+                currentProjectId = newObj.id;
+                dataProjectInput.value = null;
+                renderAndSave();
+                newProjectRender();
             }
         }
         if (btn.dataset.formSubmit == "task") {
-            if (dataTaskInput.value == null || dataTaskInput == '') {
-                return
+            if (dataTaskInput.value === null || dataTaskInput === " ") {
+                return;
             } else {
-                let newObj = new Task(dataTaskInput.value, dataDateInput.value)
-                let currentProject = findCurrentProject(projects)
-                currentProject.tasks.push(newObj)
-                newObj.id = currentProjectId
-                dataTaskInput.value = null
-                dataDateInput.value = null
-                renderAndSave()
+                let newObj = new Task(dataTaskInput.value, dataDateInput.value);
+                let currentProject = findCurrentProject(projects);
+                currentProject.tasks.push(newObj);
+                newObj.id = currentProjectId;
+                dataTaskInput.value = null;
+                dataDateInput.value = null;
+                renderTasks();
+                save();
             }
         }
-    })
-})
+    });
+});
 
-dataCancelFormBtn.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        e.preventDefault()
-        closeForms()
-    })
-})
+dataCancelFormBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        closeForms();
+    });
+});
 
-dataDeleteProjectBtn.forEach(btn => {
-    btn.addEventListener('click', () => {
+dataDeleteProjectBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
         if (btn.dataset.deleteProject == "first-check") {
-            document.querySelector('.confirm-delete').classList.toggle('hide')
+            document.querySelector(".confirm-delete").classList.toggle("hide");
         }
         if (btn.dataset.deleteProject == "confirm") {
-            let index = getIndex(findCurrentProject(projects))
-            document.querySelector('.confirm-delete').classList.toggle('hide')
-            projects.splice(index, 1)
-            renderAndSave()
+            let index = getIndex(findCurrentProject(projects));
+            document.querySelector(".confirm-delete").classList.toggle("hide");
+            projects.splice(index, 1);
+            renderAndSave();
+            deleteButtonRender();
         }
-    })
-})
+    });
+});
 
 class Project {
     constructor(name) {
-        this.name = name
+        this.name = name;
         this.tasks = [];
         this.id = Date.now().toString();
-        this.classList = "project-name";
+        this.classList = "project-name active";
     }
 }
 
 class Task {
     constructor(name, dueDate) {
-        this.name = name
-        this.dueDate = dueDate
-        this.complete = false
-        this.classList = "task-name"
+        this.name = name;
+        this.dueDate = dueDate;
+        this.complete = false;
+        this.classList = "task-name";
     }
 }
 
-renderAndSave()
-console.log(projects)
+renderAndSave();
